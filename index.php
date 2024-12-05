d58bf10031fb5170e48b204c04a576ab5ca1265d
Habib Luqman <habibluqman67@gmail.com>
2024-12-05 16:09:06 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -8,5 +8,10 @@
 <body>
     <h1>hello World!</h1>
     <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Illo itaque nisi odit nesciunt, beatae animi similique, minima optio aut dicta ea exercitationem veritatis enim, ullam repellat! Aut ducimus optio in nisi! Corrupti sed non adipisci neque aut ea odio molestiae incidunt magnam repudiandae eius eligendi maxime excepturi commodi exercitationem, minus dolorem? Omnis ex cupiditate mollitia commodi, at, nostrum cum delectus aliquam ab voluptatum, tenetur tempora ullam debitis accusamus quos veniam fuga nam numquam magnam? Ipsum, qui esse.</p>
+    <ul>
+        <li>Haha</li>
+        <li>Hihi</li>
+        <li>Hoho</li>
+    </ul>
 </body>
 </html>
